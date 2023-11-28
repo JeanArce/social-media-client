2760883f217f135e520f7ca1573c5eda2306baeb
JeanArce <113121403+JeanArce@users.noreply.github.com>
2023-11-28 13:47:52 +0100

    revert test fix

--- cypress/e2e/assignment/login.cy.js
+++ cypress/e2e/assignment/login.cy.js
@@ -8,9 +8,7 @@ describe('login', () => {
     });
     cy.tick(1000);
     cy.get('#loginEmail').type('TEST_EMAIL', { delay: 500 });
-    cy.get('#loginPassword').type('TEST_PASSWORD', { delay: 500 });
-
-    cy.get('form#log-in').submit();
+    cy.get('#loginPassword').type('TEST_PASSWORD{enter}', { delay: 500 });
 
     cy.window().its('localStorage.token').should('exist');
   });
